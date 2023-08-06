a00d290d3e4bc7fda367a21bc26efa89a2b1f5e8
ayt_home <y00913@naver.com>
2023-08-06 13:52:32 +0900

    feat: ip key log 추가

--- back-end/src/main/java/com/example/backend/interceptor/SingleVisitInterceptor.java
+++ back-end/src/main/java/com/example/backend/interceptor/SingleVisitInterceptor.java
@@ -3,6 +3,7 @@ package com.example.backend.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 
 import com.example.backend.utils.ClientUtils;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SingleVisitInterceptor implements HandlerInterceptor {
@@ -28,6 +30,7 @@ public class SingleVisitInterceptor implements HandlerInterceptor {
             ip = ip.substring(0,ip.indexOf(","));
         }
         String key = ip + "_" + today;
+        log.info("key : " + key);
 
         ValueOperations valueOperations = redisTemplate.opsForValue();
 
