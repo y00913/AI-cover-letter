b34c4118735ca5c18c22c95a328d49d0c65089ce
ayt_home <y00913@naver.com>
2023-08-06 10:55:05 +0900

    feat: 헤더 확인 추가

--- back-end/src/main/java/com/example/backend/utils/ClientUtils.java
+++ back-end/src/main/java/com/example/backend/utils/ClientUtils.java
@@ -1,6 +1,10 @@
 package com.example.backend.utils;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Enumeration;
 
 public class ClientUtils {
 
@@ -25,11 +29,21 @@ public class ClientUtils {
 
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("X-Real-IP");
-            System.out.println(ip);
         }
 
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
+        }
+
+        request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = (String) headerNames.nextElement();
+                String value = request.getHeader(name);
+                System.out.println("Header ::: " + name + " : " + value);
+            }
         }
 
         return ip;
