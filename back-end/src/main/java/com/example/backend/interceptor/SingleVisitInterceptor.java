cc86a50e9f4084c5b7a936457c458ac86dd23f5c
ayt_home <y00913@naver.com>
2023-08-07 20:48:30 +0900

    feat: interceptor prehandle 변경

--- back-end/src/main/java/com/example/backend/interceptor/SingleVisitInterceptor.java
+++ back-end/src/main/java/com/example/backend/interceptor/SingleVisitInterceptor.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.AsyncHandlerInterceptor;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.time.LocalDate;
 
@@ -16,12 +16,12 @@ import com.example.backend.utils.ClientUtils;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class SingleVisitInterceptor implements AsyncHandlerInterceptor {
+public class SingleVisitInterceptor implements HandlerInterceptor {
 
     private final RedisTemplate<String, String> redisTemplate;
 
     @Override
-    public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String ip = ClientUtils.getIp(request);
         String userAgent = request.getHeader("User-Agent");
         String today = LocalDate.now().toString();
@@ -37,6 +37,8 @@ public class SingleVisitInterceptor implements AsyncHandlerInterceptor {
         if(!valueOperations.getOperations().hasKey(key)) {
             valueOperations.set(key, userAgent);
         }
+
+        return true;
     }
 
 }
