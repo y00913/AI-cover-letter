6e2f7ca484197751dd47b1410b8fe1cd74814258
y00913 <ayt0913@gmail.com>
2023-08-26 18:19:04 +0900

    feat: scheduler log 추가

--- back-end/src/main/java/com/example/backend/domain/Visitor.java
+++ back-end/src/main/java/com/example/backend/domain/Visitor.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,8 +22,11 @@ public class Visitor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+    @NotNull
     private String ip;
+    @NotNull
     private String userAgent;
+    @NotNull
     private LocalDate createAt;
 
 }
