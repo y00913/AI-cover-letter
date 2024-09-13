c7395b6416b277e2b8f467ed6bd26cc4d3bc1910
yutae <y00913@naver.com>
2024-09-13 11:41:03 +0900

    fix: reload 오류 해결

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    hot: false,       // HMR 비활성화
-    liveReload: false // 라이브 리로드 비활성화
+  configureWebpack: {
+    devServer: {
+      hot: false,
+      client: {
+        webSocketURL: false, // 웹소켓 연결 비활성화
+      }
+    }
   }
 })
