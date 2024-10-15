0a1694e545656031d68645a5f75c4026e36440aa
y00913 <ayt0913@gmail.com>
2024-10-15 21:43:31 +0900

    feat: index.html 수정

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,8 +1,4 @@
-module.exports = {
-  chainWebpack: config => {
-    config.plugin('html').tap(args => {
-      args[0].meta = { charset: 'UTF-8' };
-      return args;
-    });
-  }
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
